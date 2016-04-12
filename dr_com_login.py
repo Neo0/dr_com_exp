d95d359d44a516281b6e0258eb5396a39f34c7c6
Neo0 <wjh100@live.cn>
2016-04-12 20:14:43 +0800

    drcom login

---
+++ dr_com_login.py
@@ -0,0 +1,35 @@
+# -*-coding:utf-8-*-
+
+import urllib,httplib
+from BeautifulSoup import BeautifulSoup
+import re
+
+url = '192.168.168.168'
+
+
+def login(url,userid,passw):
+	print passw
+	postp= {'R6': 1, 'DDDDD': userid, 'upass': passw,'0MKKey': '%B5%C7%A1%A1%C2%BC'}
+	params = urllib.urlencode(postp)
+	headers = {"Content-type": "application/x-www-form-urlencoded"}
+	conn = httplib.HTTPConnection(url)
+	conn.request("POST",'/a30.htm',params,headers)
+	res = conn.getresponse().read()
+	conn.close()
+	soup = BeautifulSoup(res)
+	reg = r'''<!--     \t\t\r\nMsg=(.*?);time'''
+	id = 0
+	status = [False,False,True,True,True,False,False,False,False,False,False,False,False,False,False,True]
+	try:
+		id = int(re.findall(reg, soup.text, re.S)[0])
+	except IndexError:
+		id = 15
+		return status[id]
+
+
+
+onePwd = 
+userid = 
+if login(url,userid,onePwd):
+	print 'success!'
+
